e596bccff03208f6ff1e48aeb2e93068050d0e33
herquiloide <herquiloide@gmail.com>
2020-12-17 21:23:14 +0200

    bot concluido

---
+++ script.js
@@ -0,0 +1,29 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch({ headless: false, devtools: true });
+  const page = await browser.newPage();
+  await page.goto('https://afazenda.r7.com/a-fazenda-12/votacao');
+
+  let votos = 0
+
+  setInterval(async () => {
+    try {
+      await page.click('[data-id="302"]');
+      await page.click('.voting-button');
+
+      await page.evaluate(() => {
+        let teste = document.querySelector('.voting-modal__close')
+        console.log( teste )
+        $(teste).click()
+      })
+
+      votos++
+      console.log(`Você votou: ${votos} vezes`);
+
+    } catch( err) {
+      console.error('ERROR => ', err);
+    }
+
+  },1500)
+})();
